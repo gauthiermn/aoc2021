e6b4fa54ab803f159c14181dc58dded113157451
Douglas Gauthier <Douglas.Gauthier@Emerson.com>
2021-12-03 13:27:52 -0600

    day3-part2

--- day3/main2.js
+++ day3/main2.js
@@ -1,29 +1,25 @@
 var fs = require('fs'), filename = 'input.txt';
 fs.readFile(filename, 'utf8', function (err, data) {
     if (err) throw err;
-    var depth = 0, position = 0;
 
     var d = data.split('\n');
-    /* .map(s => {
-        return parseInt(s, 2)
-    });*/
-    var a = [];
-    for (let i = 0; i < 12; i++) {
-        var v = d.reduce((p, c) => {
-            if (c.slice(i, i + 1) === '0') { return p + 1 } else { return p - 1 };
-        }, 0);
-        if (v > 0) { v = '1' } else { v = '0' }
-        a.push(v);
-    }
-    console.log(a.join(""));
+    var oxy, co2;
 
-    var gamma = parseInt(a.join(""), 2);
-    console.log(a.map(s => { if (s === '1') { return '0' } else { return '1' } }).join(""));
-    var eps = parseInt(a.map(s => { if (s === '1') { return '0' } else { return '1' } }).join(""), 2);
+    oxy = findSingleReading(d, true);
+    co2 = findSingleReading(d, false);
 
-
-    console.log(gamma);
-    console.log(eps);
-    console.log(eps * gamma);
+    console.log('Oxygen: %d', oxy);
+    console.log('CO2: %d', co2);
+    console.log('Product: %d', oxy * co2);
 
 });
+
+function findSingleReading(data, high) {
+    var v;
+    for (let i = 0; i < 12; i++) {
+        v = data.reduce((p, c) => { if (c.slice(i, i + 1) === '0') { return p + 1 } else { return p - 1 } }, 0);
+        if (v > 0) { v = high ? '1' : '0' } else { v = high ? '0' : '1' }
+        data = data.filter(s => s.slice(i, i + 1) === v);
+        if (data.length === 1) { return parseInt(data[0], 2) }
+    }
+}
